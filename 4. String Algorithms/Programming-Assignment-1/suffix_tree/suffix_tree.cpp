dd60b71937644aa4a5671d21979a9dc732baa8da
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2017-09-10 23:26:32 +0800

    add starter code for string algo PA 1

---
+++ 4. String Algorithms/Programming-Assignment-1/suffix_tree/suffix_tree.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::map;
+using std::string;
+using std::vector;
+
+// Build a suffix tree of the string text and return a vector
+// with all of the labels of its edges (the corresponding 
+// substrings of the text) in any order.
+vector<string> ComputeSuffixTreeEdges(const string& text) {
+  vector<string> result;
+  // Implement this function yourself
+  return result;
+}
+
+int main() {
+  string text;
+  cin >> text;
+  vector<string> edges = ComputeSuffixTreeEdges(text);
+  for (int i = 0; i < edges.size(); ++i) {
+    cout << edges[i] << endl;
+  }
+  return 0;
+}
---
+++ 4. String Algorithms/Programming-Assignment-1/trie_matching/trie_matching.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int const Letters =    4;
+int const NA      =   -1;
+
+struct Node
+{
+	int next [Letters];
+
+	Node ()
+	{
+		fill (next, next + Letters, NA);
+	}
+
+	bool isLeaf () const
+	{
+	    return (next[0] == NA && next[1] == NA && next[2] == NA && next[3] == NA);
+	}
+};
+
+int letterToIndex (char letter)
+{
+	switch (letter)
+	{
+		case 'A': return 0; break;
+		case 'C': return 1; break;
+		case 'G': return 2; break;
+		case 'T': return 3; break;
+		default: assert (false); return -1;
+	}
+}
+
+vector <int> solve (const string& text, int n, const vector <string>& patterns)
+{
+	vector <int> result;
+
+	// write your code here
+
+	return result;
+}
+
+int main (void)
+{
+	string t;
+	cin >> text;
+
+	int n;
+	cin >> n;
+
+	vector <string> patterns (n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> patterns[i];
+	}
+
+	vector <int> ans;
+	ans = solve (t, n, patterns);
+
+	for (int i = 0; i < (int) ans.size (); i++)
+	{
+		cout << ans[i];
+		if (i + 1 < (int) ans.size ())
+		{
+			cout << " ";
+		}
+		else
+		{
+			cout << endl;
+		}
+	}
+
+	return 0;
+}
