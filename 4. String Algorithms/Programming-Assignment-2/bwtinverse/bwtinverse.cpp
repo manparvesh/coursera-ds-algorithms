29f628795b453f804595b36038db45c11d4830a1
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2017-09-12 18:28:16 +0800

    PA 2

--- 4. String Algorithms/Programming-Assignment-2/bwtinverse/bwtinverse.cpp
+++ 4. String Algorithms/Programming-Assignment-2/bwtinverse/bwtinverse.cpp
@@ -11,18 +11,26 @@ using std::vector;
 
 #define pb push_back
 
+void print_vector(vector<string> v) {
+    for (string s : v) {
+        std::cout << s << endl;
+    }
+}
+
 string InverseBWT(const string& bwt) {
     string text = "";
 
     // write your code here
     vector<string> v(bwt.length(), "");
-    int x = bwt.length();
-    while (x--) {
-        for (int i = 0; i < bwt.length(); i++) {
-            v[i] = bwt[i] + v[i];
-        }
-        sort(v.begin(), v.end());
+    for (int i = 0; i < bwt.length(); i++) {
+        v[i] = bwt[i] + v[i];
     }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < bwt.length(); i++) {
+        v[i] = bwt[i] + v[i];
+    }
+
+    print_vector(v);
 
     text = v[0];
     text.erase(text.begin(), text.begin() + 1);
--- 4. String Algorithms/Programming-Assignment-2/suffix_array/suffix_array.cpp
+++ 4. String Algorithms/Programming-Assignment-2/suffix_array/suffix_array.cpp
@@ -1,16 +1,6 @@
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <utility>
+#include <bits/stdc++.h>
+using namespace std;
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::make_pair;
-using std::pair;
-using std::string;
-using std::vector;
 
 // Build suffix array of the string text and
 // return a vector result of the same length as the text
@@ -18,18 +8,29 @@ using std::vector;
 // in text where the i-th lexicographically smallest
 // suffix of text starts.
 vector<int> BuildSuffixArray(const string& text) {
-  vector<int> result;
-  // Implement this function yourself
-  return result;
+    vector<int> result;
+    // Implement this function yourself
+
+    std::vector<pair<string, int>> v;
+    for (int i = 0; i < text.length(); i++) {
+        v.push_back(make_pair(text.substr(i), i));
+    }
+
+    sort(v.begin(), v.end());
+    for (auto i : v) {
+        result.push_back(i.second);
+    }
+
+    return result;
 }
 
 int main() {
-  string text;
-  cin >> text;
-  vector<int> suffix_array = BuildSuffixArray(text);
-  for (int i = 0; i < suffix_array.size(); ++i) {
-    cout << suffix_array[i] << ' ';
-  }
-  cout << endl;
-  return 0;
+    string text;
+    cin >> text;
+    vector<int> suffix_array = BuildSuffixArray(text);
+    for (int i = 0; i < suffix_array.size(); ++i) {
+        cout << suffix_array[i] << ' ';
+    }
+    cout << endl;
+    return 0;
 }
